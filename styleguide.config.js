0de75e4492e9d3659956bec86bab11836e2a4576
Gitlab-CI <dev@kopaxgroup.com>
2018-12-05 12:22:07 +0700

    fix(loaders): fixes loaders and using itself as testing

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,2 +1,7 @@
 const { createConfig } = require('@rollup-umd/documentation');
-module.exports = createConfig();
+module.exports = createConfig(undefined, {
+  loaders: require('./src/loaders'), // eslint-disable-line global-require
+  loader: 'yeutech',
+  favicons: require('./src/favicons'), // eslint-disable-line global-require
+  favicon: 'yeutech',
+});
